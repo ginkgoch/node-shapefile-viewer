0b13a32c8dd1958c0319599f5044081b57e0c93e
ginkgoch <ginkoch@outlook.com>
2018-08-08 01:38:39 +0800

    maximum size

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({ width: 1024, height: 768, show: false })
+  win.maximize()
+  win.show()
 
   // and load the index.html of the app.
   win.loadFile('./assets/main.html')
