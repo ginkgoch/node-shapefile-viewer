e03024b0c76d293f4f80a355b1e18f929722cb2e
ginkgoch <ginkoch@outlook.com>
2018-08-09 22:41:06 +0800

    19: removing leancloud saves a lot of space

--- assets/storage/MessageBoard.js
+++ assets/storage/MessageBoard.js
@@ -1,4 +1,4 @@
-const AV = require('leancloud-storage');
+const AV = require('../dep/av-min');
 AV.init('yeqsBIYWYlwqRjFiy3o76aPO-gzGzoHsz', 'gRGNmsiwQKFRzLVytGDE3E5h');
 
 module.exports = AV.Object.extend('Message_Board');
